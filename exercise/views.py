d0c3bef17bb4048def4a9241fac351794ac70703
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-09 20:27:43 +0100

    Add(exercise): exercise list items

--- exercise/views.py
+++ exercise/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Exercise
 
 def exercise_list(request):
     """
     Renders the exercise list page
     """
-    return render(request, "exercise/exercise.html")
+    exercises = Exercise.objects.all()
+    return render(request, "exercise/exercise.html", {"exercises": exercises})
