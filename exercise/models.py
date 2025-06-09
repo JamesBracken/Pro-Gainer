0958b02762012cc29f3790abea0d8cde753c885a
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-09 19:10:40 +0100

    Add(exercise): exercise model

--- exercise/models.py
+++ exercise/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Exercise(models.Model):
+    exercise_title = models.CharField(unique=True)
+    image = CloudinaryField('image')
+    difficulty = models.CharField()
+    intensity = models.CharField()
+    instruction = models.TextField()
+    equipment = models.CharField()
+    targeted_muscles = models.CharField()
+    indirectly_targeted_muscles = models.CharField()
+    slug = models.SlugField()
