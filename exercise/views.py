336eabcd907aefc4f1334a438255f2983728ba15
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-11 17:50:19 +0100

    Add(exercise): exercise_detail view

--- exercise/views.py
+++ exercise/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Exercise
 
 def exercise_list(request):
@@ -7,3 +7,10 @@ def exercise_list(request):
     """
     exercises = Exercise.objects.all()
     return render(request, "exercise/exercise.html", {"exercises": exercises})
+
+def exercise_detail(request, exercise_slug):
+    """
+    Renders the exercise list page
+    """
+    exercise = get_object_or_404(Exercise, slug=exercise_slug)
+
