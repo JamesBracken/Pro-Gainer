84fb857b4160d3e3119eb68bd28c4d9e990a06af
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-13 22:24:28 +0100

    Add(membership): views.py add checkout POST block

--- membership/views.py
+++ membership/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SubscribeForm
 
 # Create your views here.
@@ -8,7 +8,14 @@ def checkout(request):
     Render the subscription form for user checkout
     """
     if request.method == "POST":
-        return
+        subscription_form = SubscribeForm(request.POST)
+        if subscription_form.is_valid():
+            subscription = subscription_form.save(commit=False)
+            subscription.user = request.user
+            subscription.membership_start = None
+            subscription.next_payment_date = None
+            subscription_form.save()
+            return redirect("home")
     else:
         subscription_form = SubscribeForm()
         template = "membership/subscription_form.html"
