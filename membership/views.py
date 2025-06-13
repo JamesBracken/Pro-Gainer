64aec0e1710f7b33393d453cc840efa18146be38
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-13 19:41:04 +0100

    Add(membership): views.py checkout view

--- membership/views.py
+++ membership/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import SubscribeForm
 
 # Create your views here.
+
+def checkout(request):
+    """
+    Render the subscription form for user checkout
+    """
+    if request.method == "POST":
+        return
+    else:
+        subscription_form = SubscribeForm()
+        template = "membership/subscription_form.html"
+        context = {
+            "subscription_form": subscription_form,
+        }
+
+        return render(request, template, context)
