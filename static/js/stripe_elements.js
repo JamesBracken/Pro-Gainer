e30e186bf7c26af96ccf11000faa11b1cc04687e
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-17 14:59:19 +0100

    Add(membership): stripe_elements.js scripts

---
+++ static/js/stripe_elements.js
@@ -0,0 +1,24 @@
+// The javascript in this file was done with guidance from
+// The code institute boutique ado guide https://www.youtube.com/watch?v=eUcMh5s_27I&t=456s
+const stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+let clientSecret = $("#id_client_secret").text().slice(1, -1);
+const stripe = Stripe(stripePublicKey);
+const elements = stripe.elements();
+
+const style = {
+  base: {
+      color: '#000',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+          color: '#aab7c4'
+        }
+    },
+    invalid: {
+    color: '#dc3545',
+    iconColor: '#dc3545'
+}
+};
+const card = elements.create("card", {style: style});
+card.mount("#card-element");
