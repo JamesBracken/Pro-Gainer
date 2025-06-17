a7ca26466ed0e0406f8bca66febe8242bde97c65
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-17 20:14:45 +0100

    Add(membership): view.checkout stripe values

--- membership/views.py
+++ membership/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import SubscribeForm
 from django.conf import settings
+from django.contrib import messages
 
+import stripe
 # Create your views here.
 
 def checkout(request):
@@ -9,6 +11,19 @@ def checkout(request):
     Render the subscription form for user checkout
     """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    ADD_TOTAL_AMOUNT_HERE_FOR_PAYMENT = 120
+    stripe.api_key =  stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=ADD_TOTAL_AMOUNT_HERE_FOR_PAYMENT,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+                         Please make sure the public key is set.')
+
     if request.method == "POST":
         subscription_form = SubscribeForm(request.POST)
         if subscription_form.is_valid():
@@ -24,7 +39,7 @@ def checkout(request):
         context = {
             "subscription_form": subscription_form,
             "stripe_public_key": stripe_public_key,
-            "client_secret": "test client secret",
+            "client_secret": intent.client_secret,
         }
 
         return render(request, template, context)
