68ad4b6cb90a44f4ffc663e5ef505f9b56dc21b8
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-17 14:48:13 +0100

    Fix(membership): model.Migration adjust fields

--- membership/models.py
+++ membership/models.py
@@ -12,26 +12,32 @@ class Membership(models.Model):
             ("regular", "Regular"),
         ]
 
-    # General user details models is heavily based off of the Code Institute
+    # General user details models is based off of the Code Institute
     # Boutique Ado project. Obviously this information is used across
     # majority of sites so not much if any tweaking involved
     # Similarly we would like to have the user data for later uses like
     # potential ads, promotions etc.
+
+    # User details
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100, null=False, blank=False)
     email_address = models.EmailField(max_length=250, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
+    # User Address details
     country = models.CharField(max_length=100, null=False, blank=False)
     post_code = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=100, null=False, blank=False)
     street_address_1 = models.CharField(max_length=200, null=False, blank=False)
     street_address_2 = models.CharField(max_length=200, null=True, blank=True)
-    country = models.CharField(max_length=70, null=False, blank=False)
-
-    # The below models are for use in the functionality of the website
+    county = models.CharField(max_length=70, null=False, blank=False)
+    
+    # Some of the below models are for use in the functionality of the website
     # and potential future upgrades
+    # Membership details
+    membership_start = models.DateTimeField(null=True)
     gym_location = models.CharField(choices=GYM_LOCATIONS)
     membership_type = models.CharField(choices=MEMBERSHIP_TYPES)
     is_member_active = models.BooleanField(default=False)
-    membership_start = models.DateTimeField(null=True)
     next_payment_date = models.DateTimeField(null=True)
+
+
