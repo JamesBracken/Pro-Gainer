52986f3780e34946eb9bcbc078f22fc364fbe3ef
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-17 18:44:17 +0100

    Add(Membership): stripe_elements.js card validation handling

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -7,18 +7,36 @@ const elements = stripe.elements();
 
 const style = {
   base: {
-      color: '#000',
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSmoothing: 'antialiased',
-      fontSize: '16px',
-      '::placeholder': {
-          color: '#aab7c4'
-        }
-    },
-    invalid: {
+    color: '#000',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
     color: '#dc3545',
     iconColor: '#dc3545'
-}
+  }
 };
-const card = elements.create("card", {style: style});
+const card = elements.create("card", { style: style });
 card.mount("#card-element");
+
+// Handle the card element validation errors
+
+card.addEventListener("change", function (event) {
+  const errorDiv = document.getElementById("card-errors");
+  if (event.error) {
+    const html = `
+    <span class="icon: role="alert">
+    <i class="fas fa-times"></i>
+    </span>
+    <span>${event.error.message}</span>`
+
+    $(errorDiv).html(html);
+  } else {
+    errorDiv.textContent = "";
+  }
+  
+})
