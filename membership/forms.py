d49f81915cc1811e136c959d4447def31f5dee04
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-17 18:25:20 +0100

    Add(membership): forms.SubscribeForm assign field classes

--- membership/forms.py
+++ membership/forms.py
@@ -6,8 +6,6 @@ class SubscribeForm(forms.ModelForm):
     """
     Creates a form for :model:`membership.Membership`
     """
-
-
     class Meta:
         model = Membership
         fields = (
@@ -23,22 +21,11 @@ class SubscribeForm(forms.ModelForm):
             "gym_location",
             "membership_type",
         )
-
-    # "user"
-    # ,"full_name"
-    # ,"email_address"
-    # ,"phone_number"
-    # ,"country"
-    # ,"post_code"
-    # ,"town_or_city"
-    # ,"street_address_1"
-    # ,"street_address_2"
-    # ,"country"
-
-    # # The below models are for use in the functionality of the website
-    # # and potential future upgrades
-    # gym_location
-    # membership_type
-    # is_member_active
-    # membership_start
-    # next_payment_date
+# for loop to add a class to each field
+    def __init__(self, *args, **kwargs):
+        """
+        Add classes to each field
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "stripe-style-input"
