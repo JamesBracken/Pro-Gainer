ad30ea9957552e7b72693936f988a626858d9fb2
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-17 14:58:27 +0100

    Add(settings): stripe keys

--- pro_gainer/settings.py
+++ pro_gainer/settings.py
@@ -182,3 +182,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe 
+
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
+STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
