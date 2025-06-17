aeee96b3074738276e79b49ff97751d7c11934dd
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-17 20:47:21 +0100

    Add(membership) views.checkout pass instance if it exists

--- membership/views.py
+++ membership/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SubscribeForm
+from .models import Membership
 from django.conf import settings
 from django.contrib import messages
 
@@ -10,22 +11,28 @@ def checkout(request):
     """
     Render the subscription form for user checkout
     """
+    # Checkout code was done alongside the code institute
+    # Boutique ado project and tweaked for the needs of this project
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
-    ADD_TOTAL_AMOUNT_HERE_FOR_PAYMENT = 120
+    ADD_TOTAL_AMOUNT_HERE_FOR_PAYMENT = 420
     stripe.api_key =  stripe_secret_key
     intent = stripe.PaymentIntent.create(
         amount=ADD_TOTAL_AMOUNT_HERE_FOR_PAYMENT,
         currency=settings.STRIPE_CURRENCY,
     )
-    
-
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
                          Please make sure the public key is set.')
+    is_membership_instance = Membership.objects.filter(user=request.user).exists()
 
     if request.method == "POST":
-        subscription_form = SubscribeForm(request.POST)
+        if (is_membership_instance):
+            membership_instance = Membership.objects.filter(user=request.user).first()
+            subscription_form = SubscribeForm(request.POST, instance=membership_instance)
+        else:
+            subscription_form = SubscribeForm(request.POST)
+
         if subscription_form.is_valid():
             subscription = subscription_form.save(commit=False)
             subscription.user = request.user
@@ -34,7 +41,12 @@ def checkout(request):
             subscription_form.save()
             return redirect("home")
     else:
-        subscription_form = SubscribeForm()
+        if (is_membership_instance):
+            membership_instance = Membership.objects.filter(user=request.user).first()
+            subscription_form = SubscribeForm(instance=membership_instance)
+        else:
+            subscription_form = SubscribeForm()
+        
         template = "membership/subscription_form.html"
         context = {
             "subscription_form": subscription_form,
