1a731b7f477028eebe7a1ab5951d7150ee4dc1f2
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-21 22:42:42 +0100

    Add(membership): webhoo_handler more handlers

--- membership/urls.py
+++ membership/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('membership_form/', views.checkout, name='membership_form'),
     path('checkout_success/', views.checkout_success, name='checkout_success'),
     path('store_membership_length/', views.store_membership_length, name='store_membership_length'),
     path('my_profile/', views.my_profile, name='my_profile'),
+    path('wh/', webhook, name='webhook'),
 ]
