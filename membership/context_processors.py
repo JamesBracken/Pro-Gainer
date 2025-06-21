9a1cebe96919df262a9c79284f841b88f1b97834
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2025-06-21 17:11:41 +0100

    Add(membership): adjust context processor to user is_user_membership_active util

--- membership/context_processors.py
+++ membership/context_processors.py
@@ -1,9 +1,14 @@
-from .models import Membership
-from django.utils import timezone
+from .utils import is_user_membership_active
 
 def check_member_status(request):
-    if request.user.is_authenticated:
-        membership = Membership.objects.filter(user=request.user).first()
-        if membership and membership.membership_end_date >= timezone.now():
-            return {"is_member_active": True }
-    return {"is_member_active": False }
+    """
+    Returns a boolean value using the logic in 
+    utils.is_user_membership_active
+    
+    **Context**
+    
+    ``is_member_active``
+        Stores a boolean value based on the instance stored in
+        :model:`membership.Membership`
+    """
+    return {"is_member_active": is_user_membership_active(request.user)}
